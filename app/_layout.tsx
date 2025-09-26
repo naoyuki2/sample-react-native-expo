b2bc19ceaa90d68c63f79410d587e9fd0f650177
naoyuki2 <kamaboko912@gmail.com>
2025-09-26 19:39:09 +0900

    add react-native-paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === "dark" ? MD3DarkTheme : MD3LightTheme;
+
+  return (
+    <PaperProvider theme={theme}>
+      <Stack />
+    </PaperProvider>
+  );
 }
